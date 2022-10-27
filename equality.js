19d2fcb24a214893411bc1808e8e6a69ec8ac06c
Nikhil gupta <61626746+ni774@users.noreply.github.com>
2022-10-27 11:24:13 +0530

    Create equality.js

---
+++ equality.js
@@ -0,0 +1,7 @@
+// abstract equality
+"6"==6
+//give true because it does type checking if not then coercion
+
+//strict equality
+"6"===6
+//give false because it does type checking if not then false
